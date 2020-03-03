4953780d03cd2869d3ed0d4b7ca47fe6b4eba51b
Rob Fonseca <rob@kirschbaumdevelopment.com>
2020-03-03 14:58:22 -0500

    Revert 7.0 support

--- src/MandrillServiceProvider.php
+++ src/MandrillServiceProvider.php
@@ -8,7 +8,7 @@ class MandrillServiceProvider extends ServiceProvider
 {
     public function boot()
     {
-        $this->app['swift.transport']->extend('mandrill', function () {
+        $this->app['mail.manager']->extend('mandrill', function () {
             $config = $this->app['config']->get('services.mandrill', []);
 
             return new MandrillTransport(new \GuzzleHttp\Client($config), $config['secret']);
