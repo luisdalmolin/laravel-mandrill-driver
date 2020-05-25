7df6f93a646892adbbcd7114b6c9b516e670d1ef
Theo Gibbons <theo_112@hotmail.com>
2020-05-26 10:04:22 +1200

    Swap `extend` in the `boot` method to `resolving` in the `register` method

--- src/MandrillServiceProvider.php
+++ src/MandrillServiceProvider.php
@@ -6,12 +6,13 @@ use Illuminate\Support\ServiceProvider;
 
 class MandrillServiceProvider extends ServiceProvider
 {
-    public function boot()
+    public function register()
     {
-        $this->app['mail.manager']->extend('mandrill', function () {
-            $config = $this->app['config']->get('services.mandrill', []);
-
-            return new MandrillTransport(new \GuzzleHttp\Client($config), $config['secret']);
+        $this->app->resolving(\Illuminate\Mail\MailManager::class, function (\Illuminate\Mail\MailManager $mail_manager) {
+            $mail_manager->extend("mandrill", function () {
+                $config = $this->app['config']->get('services.mandrill', []);
+                return new \LaravelMandrill\MandrillTransport(new \GuzzleHttp\Client($config), $config['secret']);
+            });
         });
     }
 }
