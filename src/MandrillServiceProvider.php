9d11e3ad1a0c036966bc2186abd19618d445a0a8
Jürgen van Dijk <jurgen@activecreations.nl>
2020-09-15 12:17:51 +0200

    Fix `unsupported maildriver Mandrill` warning when using fedeisas/laravel-mail-css-inliner.

--- src/MandrillServiceProvider.php
+++ src/MandrillServiceProvider.php
@@ -2,16 +2,20 @@
 
 namespace LaravelMandrill;
 
+use Illuminate\Mail\MailManager;
 use Illuminate\Support\ServiceProvider;
 
 class MandrillServiceProvider extends ServiceProvider
 {
-    public function boot()
+    public function register()
     {
-        $this->app['mail.manager']->extend('mandrill', function () {
-            $config = $this->app['config']->get('services.mandrill', []);
-
-            return new MandrillTransport(new \GuzzleHttp\Client($config), $config['secret']);
+        $this->app->resolving(MailManager::class, function (MailManager $manager) {
+            $manager->extend('mandrill', function () {
+                $config = $this->app['config']->get('services.mandrill', []);
+                return new \LaravelMandrill\MandrillTransport(
+                    new \GuzzleHttp\Client($config), $config['secret']
+                );
+            });
         });
     }
 }
