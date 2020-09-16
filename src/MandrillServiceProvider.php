d4f59dea7832abd2b023cac4245eef21d6174a7c
Rob Fonseca <robfonseca@gmail.com>
2020-09-16 07:35:14 -0400

    update boot to register

--- src/MandrillServiceProvider.php
+++ src/MandrillServiceProvider.php
@@ -9,7 +9,7 @@ use Illuminate\Support\ServiceProvider;
 
 class MandrillServiceProvider extends ServiceProvider
 {
-    public function boot()
+    public function register()
     {
         $this->app->resolving(MailManager::class, function (MailManager $manager) {
             $manager->extend('mandrill', function () {
